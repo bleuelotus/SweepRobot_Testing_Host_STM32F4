fde02d02fef91a3a86dd74aea702b2fb2ae3d165
bleuelotus <bleuelotus@163.com>
2015-11-04 10:50:16 +0800

    Added all project files into repository

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SweepRobot_Testing_Host_STM32F4' 
+ * Target:  'SweepRobot_Testing_Host_STM32F4 Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STARTUP_STM32F4xx    /* Device Startup for STM32F4 */
+
+#endif /* RTE_COMPONENTS_H */
